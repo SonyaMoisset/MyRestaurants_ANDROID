b1b6d8dce5ecddd3eb0809dc3511281f8549ee02
smoisset <smoisset@worldremit.com>
2017-11-15 21:44:40 +0000

    Add EditText

--- app/src/main/java/com/example/android/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/android/myrestaurants/MainActivity.java
@@ -3,28 +3,38 @@ package com.example.android.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    // Add a member variable to hold findRestaurantsButton to access it inside all the methods
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set the mFindRestaurantsButton variable
-        // (Button) typecasts the view as Button
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
 
         // Add a click listener
-        // setOnclickListener() method takes a new OnClickListener as a parameter
+        // setOnclickListener() takes a new OnClickListener as a parameter
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+                // Save into a String variable the inputted value
+                // getText() returns an editable data type
+                // toString(0 converts it
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+
                 // Create a new intent
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
 
