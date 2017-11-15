0bc20778ebeb136235907f4d84e171de71449d0a
smoisset <smoisset@worldremit.com>
2017-11-15 21:28:18 +0000

    Add a click listener and toast

--- app/src/main/java/com/example/android/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/android/myrestaurants/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.android.myrestaurants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // Add a member variable to hold findRestaurantsButton to access it inside all the methods
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the mFindRestaurantsButton variable
+        // (Button) typecasts the view as Button
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+
+        // Add a click listener
+        // setOnclickListener() method takes a new OnClickListener as a parameter
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add a Toast
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
