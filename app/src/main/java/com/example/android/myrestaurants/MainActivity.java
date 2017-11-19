0be535decd6553dfe92937298e9e764f20fa76c2
smoisset <smoisset@worldremit.com>
2017-11-19 17:51:51 +0000

    Add Custon Fonts

--- app/src/main/java/com/example/android/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/android/myrestaurants/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.android.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+
+        // Define the member variable at runtime in onCreate()
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        // Define our new font as Typeface type and call createFromAsset() to reference the .ttf font file
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        // Call setTypeface() on the TextView
+        mAppNameTextView.setTypeface(ostrichFont);
 
         // Add a click listener
         // setOnclickListener() takes a new OnClickListener as a parameter
