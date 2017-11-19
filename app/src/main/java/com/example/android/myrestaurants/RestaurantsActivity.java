488dff1699ee0a076baf0e5f64d05841d16985ef
smoisset <smoisset@worldremit.com>
2017-11-19 21:29:21 +0000

    Implement ListView and ArrayAdapter

--- app/src/main/java/com/example/android/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/android/myrestaurants/RestaurantsActivity.java
@@ -3,11 +3,26 @@ package com.example.android.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RestaurantsActivity extends AppCompatActivity {
 
     private TextView mLocationTextView;
+    private ListView mListView;
+
+    // Create a string array of restaurants
+    private String[] restaurants = new String[] {
+            "Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +30,21 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
 
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        mListView = (ListView) findViewById(R.id.listView);
+
+        // Add an OnItemClickListener to display a toast with the restaurant name
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Create an ArrayAdapter
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        // Set the ListView adapter to the new adapter
+        mListView.setAdapter(adapter);
 
         // Pull out the data of the intent extra
         // getIntent() to recreate the Intent
